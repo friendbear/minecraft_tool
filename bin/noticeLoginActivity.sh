423a63d8624ee27b197578ae9cd942a7efda8887
friendbear <kumagai@bear.plala.or.jp>
2016-12-23 06:57:55 +0000

    minecraft serverツール

---
+++ bin/noticeLoginActivity.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eux
+
+TMP_ROOT=/home/tomohiro_kumagai/tmp
+# 実行時の日時から、何秒前のレコードをSlack通知の対象とするか
+INTERVAL=5
+# ログファイルのフルパス
+#LOGFILENAME=$1
+LOGFILENAME=/home/minecraft/logs/latest.log
+
+checked_line=0
+unchecked_line=$(wc -l $LOGFILENAME | cut -d ' ' -f1)
+if [ -f $TMP_ROOT/lines.txt ]; then
+  checked_line=$(cat $TMP_ROOT/lines.txt)
+  echo $unchecked_line > $TMP_ROOT/lines.txt
+fi
+check_line=$(expr $unchecked_line - $checked_line)
+if [ $check_line -lt 0 ]; then
+  $check_line=0
+  echo $check_line > $TMP_ROOT/lines.txt
+
+fi
+(tail -n $check_line $LOGFILENAME | grep 'logged in') > $TMP_ROOT/login.txt
+
+cat $TMP_ROOT/login.txt | while read  line
+do
+
+  #if echo $line | grep "logged in"; then
+    set -- $line
+    time=$1
+    echo "$time minecraft server logged in!\n{$line}" | sh -x /home/tomohiro_kumagai/bin/webhook.sh
+  #fi
+done
---
+++ bin/webhook.sh
@@ -0,0 +1,61 @@
+#!/bin/sh -x
+
+set -eu
+
+#Incoming WebHooksのURL
+WEBHOOKURL="https://hooks.slack.com/services/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"
+#メッセージを保存する一時ファイル
+cd /home/tomohiro_kumagai
+MESSAGEFILE=$(mktemp -p webhooks)
+trap "
+rm ${MESSAGEFILE}
+" 0
+
+usage_exit() {
+    echo "Usage: $0 [-m message] [-c channel] [-i icon] [-n botname]" 1>&2
+    exit 0
+}
+
+while getopts c:i:n:m: opts
+do
+    case $opts in
+        c)
+            CHANNEL=$OPTARG
+            ;;
+        i)
+            FACEICON=$OPTARG
+            ;;
+        n)
+            BOTNAME=$OPTARG
+            ;;
+        m)
+            MESSAGE=$OPTARG"\n"
+            ;;
+        \?)
+            usage_exit
+            ;;
+    esac
+done
+#slack 送信チャンネル
+CHANNEL=${CHANNEL:-"#notification"}
+#slack 送信名
+BOTNAME=${BOTNAME:-"minecraft"}
+#slack アイコン
+FACEICON=${FACEICON:-":ghost:"}
+#見出しとなるようなメッセージ
+MESSAGE=${MESSAGE:-""}
+
+if [ -p /dev/stdin ] ; then
+    #改行コードをslack用に変換
+    cat - | tr '\n' '\\' | sed 's/\\/\\n/g'  > ${MESSAGEFILE}
+else
+    echo "nothing stdin"
+    exit 1
+fi
+
+WEBMESSAGE='```'`cat ${MESSAGEFILE}`'```'
+
+#Incoming WebHooks送信
+curl -s -S -X POST --data-urlencode "payload={\"channel\": \"${CHANNEL}\", \"username\": \"${BOTNAME}\", \"icon_emoji\": \"${FACEICON}\", \"text\": \"${MESSAGE}${WEBMESSAGE}\" }" ${WEBHOOKURL} >/dev/null
+
+
